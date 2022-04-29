2f316404ae79b5413c91ba6c8f269f0d1bea87ea
Wesley Huang <wesley.huang@tw.promise.com>
2022-04-29 15:40:35 +0800

    remove everything from ProjectMgntNavigation except for VProject

--- SereneViewSample/SereneViewSample.Web/Modules/ProjectMgnt/ProjectMgntNavigation.cs
+++ SereneViewSample/SereneViewSample.Web/Modules/ProjectMgnt/ProjectMgntNavigation.cs
@@ -1,8 +1,4 @@
 ﻿using Serenity.Navigation;
 using MyPages = SereneViewSample.ProjectMgnt.Pages;
 
-[assembly: NavigationLink(int.MaxValue, "ProjectMgnt/Project", typeof(MyPages.ProjectController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "ProjectMgnt/Project Add On", typeof(MyPages.ProjectAddOnController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "ProjectMgnt/Project History", typeof(MyPages.ProjectHistoryController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "ProjectMgnt/Project Members", typeof(MyPages.ProjectMembersController), icon: null)]
-[assembly: NavigationLink(int.MaxValue, "ProjectMgnt/V Project", typeof(MyPages.VProjectController), icon: null)]
+[assembly: NavigationLink(int.MaxValue, "Project Mgnt/Project", typeof(MyPages.VProjectController), icon: null)]
